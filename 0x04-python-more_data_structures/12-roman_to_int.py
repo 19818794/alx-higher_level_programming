27334a976420b78ccacefb530e70fabed4afd5c9
19818794 <jory2ray@gmail.com>
2023-06-15 03:11:44 -0700

    Updated Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
+    """
+    converts a Roman numeral to an integer.
+    Arguments:
+        roman_string: a Roman numeral.
+    Returns:
+        converted Roman numeral.
+    """
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
-    for character in roman_string:
-        if character not in "IVXLCDM":
-            return 0
     roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                  'C': 100, 'D': 500, 'M': 1000}
     number = 0
-    for i in range(len(roman_string)):
+    len_roman_str = len(roman_string)
+    for i in range(len_roman_str):
         if roman_dic.get(roman_string[i], 0) == 0:
             return 0
-        if (i != len(roman_string) - 1) and (
+        if (i != len_roman_str - 1) and (
            roman_dic[roman_string[i]] < roman_dic[roman_string[i + 1]]):
             number += roman_dic[roman_string[i]] * -1
         else:
